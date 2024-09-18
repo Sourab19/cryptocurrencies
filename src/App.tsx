46c91bbaa25268cfbd2dba6d95454e239d19d52a
Sourab19 <sourabberlin007@gmail.com>
2024-09-19 00:13:13 +0530

    intial api request

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import './App.css';
-import axios from 'axios';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export type Crypto = {
+  id: string;
+  name: string;
+  price_usd: string;
+  symbol: string;
+  volume24: number;
+  volume24a: number;
+};
 
 function App() {
+  const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
+
+  useEffect(() => {
+    const url = "https://api.coinlore.net/api/tickers/";
+    axios(url).then((response) => {
+      setCryptos(response.data.data);
+      console.log(response.data.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      Hello
+      {" "}
+      {cryptos
+        ? cryptos.map((Crypto) => {
+            return <p>{Crypto.name + " $" + Crypto.price_usd}</p>;
+          })
+        : null}{" "}
     </div>
   );
 }
