70135cfc3dacfa406b9760c0dc97818997ef55e2
Sourab19 <sourabberlin007@gmail.com>
2024-09-19 16:22:02 +0530

    cryto summary component

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,8 @@ import React from "react";
 import "./App.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
-
-export type Crypto = {
-  id: string;
-  name: string;
-  price_usd: string;
-  symbol: string;
-  volume24: number;
-  volume24a: number;
-};
+import CryptoSummary from "./components/CryptoSummary";
+import { Crypto } from "./Types";
 
 function App() {
   const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
@@ -27,8 +20,8 @@ function App() {
     <div className="App">
       {" "}
       {cryptos
-        ? cryptos.map((Crypto) => {
-            return <p>{Crypto.name + " $" + Crypto.price_usd}</p>;
+        ? cryptos.map((crypto) => {
+            return <CryptoSummary crypto={crypto} />;
           })
         : null}{" "}
     </div>
---
+++ src/Types.tsx
@@ -0,0 +1,9 @@
+export type Crypto = {
+    id: string;
+    name: string;
+    price_usd: string;
+    symbol: string;
+    volume24: number;
+    volume24a: number;
+  };
+  
