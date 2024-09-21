35bb2a1cf3acbcb8261c0013e7b46ccb46ab7113
Sourab19 <sourabberlin007@gmail.com>
2024-09-21 18:33:48 +0530

    Pie chart

--- src/App.tsx
+++ src/App.tsx
@@ -13,38 +13,23 @@ import {
   Title,
   Tooltip,
   Legend,
+  ArcElement
 } from "chart.js";
-import { Line } from "react-chartjs-2";
+import { Pie } from "react-chartjs-2";
 import type { ChartData, ChartOptions } from "chart.js";
 import moment from "moment";
 ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
+  ArcElement,
   Tooltip,
   Legend
 );
 
 function App() {
   const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
-  const [selected, setSelected] = useState<Crypto[]>([]);
-  // const [range, setRange] = useState<number>(30);
+  const [selected, setSelected] = useState<Crypto[]>([]); 
 
-  // const [data, setData] = useState<ChartData<"line">>();
-  // const [options, setOptions] = useState<ChartOptions<"line">>({
-  //   responsive: true,
-  //   plugins: {
-  //     legend: {
-  //       display: false,
-  //     },
-  //     title: {
-  //       display: true,
-  //       text: "Chart.js Line Chart",
-  //     },
-  //   },
-  // });
+  const [data, setData] = useState<ChartData<"pie">>();
+ 
 
   useEffect(() => {
     const url =
@@ -100,7 +85,34 @@ function App() {
   // }, [selected, range]);
 
   useEffect(() => {
-    console.log("SELECTED:", selected);
+    console.log("SELECTED", selected);
+    if (selected.length === 0) return;
+    setData({
+      labels: selected.map((s) => s.name),
+      datasets: [
+        {
+          label: "# of Votes",
+          data: selected.map((s) => s.owned * s.current_price),
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(254, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132,1)",
+            "rgba(54, 162, 235,1)",
+            "rgba(255, 206, 86,1)",
+            "rgba(75, 192, 192,1)",
+            "rgba(153, 102, 2552,1)",
+            "rgba(255, 159, 64,1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    });
   }, [selected]);
 
   function updateOwned(crypto: Crypto, amount: number): void {
@@ -140,12 +152,13 @@ function App() {
         return <CryptoSummary crypto={s} updateOwned={updateOwned} />;
       })}
 
-      {/* {selected ? <CryptoSummary crypto={selected} /> : null} */}
-      {/* {data ? (
+      {/* {selected ? <CryptoSummary crypto={selected} /> : null} */} 
+      {data ? (
         <div style={{ width: 600 }}>
-          <Line options={options} data={data} />
+          <Pie data={data} />
         </div>
-      ) : null} */}
+      ) : null}
+
       {selected
         ? 'Your portfolio is worth $' + selected
             .map((s) => {
